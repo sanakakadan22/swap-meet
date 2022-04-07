501771efa9f5fd452ee2dc33c346317dcd394245
sana <sanakakadan22@yahoo.com>
2022-04-07 18:58:22 +0400

    wave1

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,7 @@
 class Item:
     pass
+    # def __init__(self, category):
+    #     self.category = ""
+
+
+    #def get_by_category(self):
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,16 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = None) :
+        if not inventory:
+            self.inventory = []
+        else:
+            self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self,item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        return False
